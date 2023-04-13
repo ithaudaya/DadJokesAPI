3d9e92008a212ab70815b023aceee243a9174af2
ithaudaya <129060687+ithaudaya@users.noreply.github.com>
2023-04-13 11:28:00 +0530

    Update IApiManager.cs

--- DadJokesAPI/IApiManager.cs
+++ DadJokesAPI/IApiManager.cs
@@ -1,11 +1,11 @@
-﻿namespace DadJokeAPI
+namespace DadJokeAPI
 {
     public interface IApiManager
     {
         Task<JokeResponse> GetRandomJokeAsync();
 
-        Task<JokeResponse> GetRandomJokesAsync(int count);
-        
+        Task<JokeResponse[]> GetRandomJokesAsync(int count);
+
         Task<JokeCountResponse> GetJokeCountAsync();
 
     }
